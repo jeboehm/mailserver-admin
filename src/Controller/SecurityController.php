5a9d87adcde045b4786a7c9771cad8860e2c86b9
Jeffrey Böhm <hello@jeffrey-boehm.de>
2020-01-07 01:13:43 +0100

    Override login page

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -37,7 +37,7 @@ class SecurityController extends AbstractController
         $error = $authenticationUtils->getLastAuthenticationError();
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('@EasyAdmin/page/login.html.twig', [
+        return $this->render('admin/login.html.twig', [
             'last_username' => $lastUsername,
             'error' => $error,
             'csrf_token_intention' => 'authenticate',
