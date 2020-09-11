488815a555d31ba56be4785ada78b628526b06c1
Jeffrey Böhm <hello@jeffrey-boehm.de>
2020-09-11 10:51:47 +0200

    Show proper error message when user has no permissions

---
+++ src/Security/AccessDeniedHandler.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of the mailserver-admin package.
+ * (c) Jeffrey Boehm <https://github.com/jeboehm/mailserver-admin>
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Security\Http\Authorization\AccessDeniedHandlerInterface;
+use Twig\Environment;
+
+class AccessDeniedHandler implements AccessDeniedHandlerInterface
+{
+    private Environment $twig;
+
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
+    public function handle(Request $request, AccessDeniedException $accessDeniedException): Response
+    {
+        return new Response($this->twig->render('admin/access_denied.html.twig'));
+    }
+}
