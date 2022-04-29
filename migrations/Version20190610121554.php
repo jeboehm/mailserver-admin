370db66f4462c6221c80b7c2c408453f6a04a5ef
Jeffrey Böhm <hello@jeffrey-boehm.de>
2022-04-29 18:31:43 +0200

    Update recipe

--- migrations/Version20190610121554.php
+++ migrations/Version20190610121554.php
@@ -8,7 +8,7 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-namespace App\Migrations;
+namespace Migrations;
 
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
